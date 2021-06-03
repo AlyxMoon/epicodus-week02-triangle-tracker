bf66517962a9ddd1c614c248a65988c2272f48db
AlyxMoon <allisterkmoon@gmail.com>
2021-06-03 13:16:14 -0700

    get HTML input in the script
    Co-authored-by: Garrett Broughton <gbrough@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $('form').submit(function (event) {
     event.preventDefault()
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
 
     console.log('do stuff')
   })
