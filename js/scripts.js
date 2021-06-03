aa28cc428ba6d7437ad998bbe8a8ab1c51e83e3f
AlyxMoon <allisterkmoon@gmail.com>
2021-06-03 13:56:17 -0700

    calculate in the script what kind of triangle we get
    Co-authored-by: Garrett Broughton <gbrough@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+function getTriangleType(side1, side2, side3) {
+  let triangleType = "something"
+
+  if (side1 === side2 && side1 === side3) {
+    triangleType = "equilateral"
+  } else if (
+    side1 === side2 ||
+    side1 === side3 ||
+    side2 === side3
+  ) {
+    triangleType = 'isosceles'
+  } else {
+    triangleType = "scalene"
+  }
+  return triangleType;
+}
+
 $(document).ready(function () {
   $('form').submit(function (event) {
     event.preventDefault()
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
-
-    console.log('do stuff')
+    const triangleType = getTriangleType(side1, side2, side3)
   })
 })
